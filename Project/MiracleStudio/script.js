f0b27dcfebbcc32ffe5f376bfb6aed8f0094b090
fairuz akhdan <fairuzakhdan01@gmail.com>
2024-12-13 00:06:41 +0700

    add project miracle studio

--- Project/MiracleStudio/script.js
+++
@@ -1,5 +0,0 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   document.querySelectorAll(".nav-item").forEach((item) => {
-//     item.addEventListener("mouseenter", (e) => {
-//       e.target.style.color = "black";
-//    
