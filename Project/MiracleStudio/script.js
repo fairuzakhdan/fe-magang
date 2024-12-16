e49d341fe09cd2f8ae599df5d52fc88e74a2a4a0
fairuz akhdan <fairuzakhdan01@gmail.com>
2024-12-16 20:30:09 +0700

    add project miracle studio

--- Project/MiracleStudio/script.js
+++ Project/MiracleStudio/script.js
@@ -1,8 +1,22 @@
 window.addEventListener('scroll',() => {
     const navbar = document.querySelector('.navbar-expand-lg');
-    if (window.scrollY > 1) {
-        navbar.style.backgroundColor = '#6c1db5';
-        // navbar.style.color = '#ffffff !important';
-        navbar.classList.add('shadow-lg');
-    }
+    const navLink = document.querySelectorAll('.nav-link');
+    navLink.forEach((nav) => {
+        // if (window.scrollY > 10) {
+        //     navbar.style.backgroundColor = '#811b9e';
+        //     nav.classList.remove('text-black');
+        //     nav.classList.add('text-white');
+        //     navbar.classList.add('shadow-lg');
+        //     navbar.style.transition = '0.5s';
+        // } else {
+        //     navbar.removeAttribute('style');
+        //     nav.classList.remove('text-white');
+        //     nav.classList.add('text-black');
+        //     navbar.classList.remove('shadow-lg');
+        //     navbar.style.transition = '0.5s';
+        // }
+    })
+    // if (window.scrollY > 1) {
+       
+    // }
 })
