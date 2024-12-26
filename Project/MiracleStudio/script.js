2f0f502b720b67799d218b1f7619110929672533
fairuz akhdan <fairuzakhdan01@gmail.com>
2024-12-26 21:50:58 +0700

    add page blog

--- Project/MiracleStudio/script.js
+++ Project/MiracleStudio/script.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+
+  
+
   const teksLogo = document.querySelector(".text-logo");
   const navbar = document.querySelector(".navbar-expand-lg");
 
@@ -41,28 +45,4 @@ document.addEventListener("DOMContentLoaded", () => {
   //         .catch((error) => console.error("Error loading navbar:", error));
   //     }
   // })
-
-  const webLink = document.getElementById("web-blog");
-
-  const updateLink = document.getElementById("update-blog");
-  const trickLink = document.getElementById("trick-blog");
-
-  const tips = document.getElementById("tips");
-  const web = document.getElementById("web");
-  const update = document.getElementById("update");
-  webLink.addEventListener("click", () => {
-    web.style.display = "block";
-    tips.style.display = "none";
-    update.style.display = "none";
-  });
-  trickLink.addEventListener("click", () => {
-    tips.style.display = "block";
-    web.style.display = "none";
-    update.style.display = "none";
-  });
-  updateLink.addEventListener("click", () => {
-    update.style.display = "block";
-    web.style.display = "none";
-    tips.style.display = "none";
-  });
 });
